75a44ffb114ab873ea332b22fd38f49854a482ce
Fatih Erikli <fatiherikli@gmail.com>
2012-05-13 03:42:06 +0300

    random quote

--- quotes/views.py
+++ quotes/views.py
@@ -1,14 +1,16 @@
 # Create your views here.
+import random
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template.context import RequestContext
 from quotes.models import Quote
 
 def home(request):
-    random_quote = Quote.objects.order_by('?')
+    # random_quote = Quote.objects.order_by('?')
+    random_quote = random.choice(Quote.objects.all())
     if not random_quote.exists():
         return HttpResponse('no data available.')
-    return HttpResponseRedirect(random_quote[0].get_absolute_url())
+    return HttpResponseRedirect(random_quote.get_absolute_url())
 
 def quote(request, id):
     quote = get_object_or_404(Quote, id=id)
