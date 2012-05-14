e2989a7bae4a6c5b54181e87d755df303499d2ea
Fatih Erikli <fatiherikli@gmail.com>
2012-05-15 00:39:45 +0300

    improved random quotes

--- quotes/views.py
+++ quotes/views.py
@@ -6,14 +6,27 @@ from django.template.context import RequestContext
 from quotes.models import Quote
 
 def home(request):
+    # TODO: use cache machine
     # random_quote = Quote.objects.order_by('?')
-    random_quote = random.choice(Quote.objects.all())
-    if not random_quote:
+    quotes = list(Quote.objects.all())
+    if not quotes:
         return HttpResponse('no data available.')
+
+    viewed_quotes = request.session.get('viewed_quotes', [])
+    if len(viewed_quotes) == len(quotes):
+        viewed_quotes = []
+
+    random_quote = random.choice(filter(
+        lambda x : not x.id in viewed_quotes,
+        quotes
+    ))
+
+    viewed_quotes.append(random_quote.id)
+    request.session['viewed_quotes'] = viewed_quotes
     return HttpResponseRedirect(random_quote.get_absolute_url())
 
 def quote(request, id):
     quote = get_object_or_404(Quote, id=id)
     return render_to_response('quote.html', context_instance=RequestContext(request, {
-        'quote' : quote
+        'quote' : quote,
     }))
